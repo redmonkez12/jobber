da643a725641040d850e569ef1fc8c859b82fd60
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-03-18 20:13:55 +0100

    add jwt

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UsersModule } from './users.module';
+import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,9 +12,14 @@ import { ConfigModule } from '@nestjs/config';
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      context: ({ req, res }) => ({
+        req,
+        res,
+      }),
       autoSchemaFile: true,
     }),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}

1613a744c27bae0b2a4764f2bc50dcfc5987d208
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-03-18 12:06:21 +0100

    Add graphql

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- apps/jobber-auth/src/app/prisma/prisma.service.ts
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from '@prisma-clients/jobber-auth';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    
+    await this.$connect();
   }
 }
-

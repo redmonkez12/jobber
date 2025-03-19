1b4c219342ad9954255d62a684c77b0cf7e71674
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-03-19 12:57:44 +0100

    Add current user

---
+++ apps/jobber-auth/src/app/auth/current-user.ts
@@ -0,0 +1,8 @@
+﻿import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) => {
+    return GqlExecutionContext.create(context).getContext().req.user;
+  }
+);
